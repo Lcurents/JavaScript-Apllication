37054ced47d4568ce61941b5b6f3216ee51b09ec
Laurentius Dika <39826578+Lcurents@users.noreply.github.com>
2023-08-21 12:19:53 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -48,10 +48,13 @@ document.getElementById("tambah").onclick=function(){
 }
 
 //tebakangka
+let percobaan=0;
 document.getElementById("submit3").onclick=function(){
+    percobaan++;
     tebakan=document.getElementById("tebakan").value;
     if (tebakan==tepat) {
         document.getElementById("ketepatan").innerHTML="Selamat "+nama+" ! Tebakan anda "+tebakan+" Adalah Benar"
+        document.getElementById("percobaan").innerHTML="Anda sudah melakukan "+percobaan+" kali percobaan";
     }else if(tebakan>tepat){
         document.getElementById("ketepatan").innerHTML="Tebakan anda "+tebakan+" Terlalu Besar"
     }else{
@@ -64,6 +67,83 @@ document.getElementById("submit4").onclick=function(){
     if(checkbox.checked){
         document.getElementById("p6").innerHTML= "Terima Kasih"
     }else{
-        document.getElementById("p6").innerHTML="Okeeh :D"
+        document.getElementById("p6").innerHTML='<a href="https://www.instagram.com/lcurents/" style="color: rgb(189, 0, 0);" target="_blank">Klik disini</a>'
     }
 }
+
+//ternary operation=shortcut untuk if/else
+let kucing;
+document.getElementById("submit5").onclick=function(){
+    //kondisi?benar:salah
+    kucing=document.getElementById("kucing").value;
+    kucing=="Yes"?document.getElementById("p7").innerHTML="Kamu Suka Kucing":document.getElementById("p7").innerHTML="Kamu Tidak Suka Kucing"
+}
+
+//temperatur suhu
+let suhu,tampil,Celcius,Fahrenheit,Kelvin;
+document.getElementById("submit6").onclick=function(){
+    Celcius=document.getElementById("Celcius");
+    Fahrenheit=document.getElementById("Fahrenheit");
+    Kelvin=document.getElementById("Kelvin");
+    //harus di convert menjadi float
+    //alasan:belum diketahui
+    suhu = parseFloat(document.getElementById("temperatur").value);
+    if(Celcius.checked){
+        tampil=suhu.toLocaleString(undefined,{style:"unit",unit:"celsius"});
+        document.getElementById("suhu").innerHTML=tampil;
+    }else if(Fahrenheit.checked){
+        tampil=suhu.toLocaleString(undefined,{style:"unit",unit:"fahrenheit"});
+        document.getElementById("suhu").innerHTML=tampil;
+    }  
+}
+
+//buah dengan menggunakan array
+let buah=[];
+let fruit;
+//tambah buah
+document.getElementById("addbuah").onclick=function(){
+    //penampilan objek
+    document.getElementById("namabuah").style.display="block"
+    document.getElementById("add").style.display="block"
+    document.getElementById("del").style.display="none"
+    //untuk menambah dari index terakhir
+    document.getElementById("push").onclick=function(){
+        fruit=document.getElementById("buah").value;
+        buah.push(fruit);
+        tampil(buah);
+    }
+    //untuk menambah dari index 0
+    document.getElementById("unshift").onclick=function(){
+        fruit=document.getElementById("buah").value;
+        buah.unshift(fruit);
+        tampil(buah);
+    }
+    function tampil(buah) {
+        document.getElementById("tampilbuah").innerHTML = buah.join(", ");
+        return;
+      }
+}
+//hapus buah
+document.getElementById("delbuah").onclick=function(){
+    //penampilan objek
+    document.getElementById("namabuah").style.display="block"
+    document.getElementById("del").style.display="block"
+    document.getElementById("add").style.display="none"
+    //untuk menambah dari index terakhir
+    document.getElementById("shift").onclick=function(){
+        fruit=document.getElementById("buah").value;
+        buah.shift(fruit);
+        tampil(buah);
+    }
+    //untuk menambah dari index 0
+    document.getElementById("pop").onclick=function(){
+        fruit=document.getElementById("buah").value;
+        buah.pop(fruit);
+        tampil(buah);
+    }
+    function tampil(buah) {
+        document.getElementById("tampilbuah").innerHTML = buah.join(", ");
+        return;
+      }
+}
+
